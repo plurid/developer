94f573da3e9552e040f91af7894c39c0434b6526
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-16 21:20:32 +0300

    developer-cli > dev: commands/status

--- packages/developer-cli/source/commands/status/index.ts
+++ packages/developer-cli/source/commands/status/index.ts
@@ -1,5 +1,8 @@
 // #region imports
     // #region external
+    import {
+        readConfigurations,
+    } from '../../services/utilities';
     // #endregion external
 // #endregion imports
 
@@ -7,7 +10,34 @@
 
 // #region module
 const status = async () => {
-    console.log('developer status');
+    const configurations = await readConfigurations();
+
+    if (configurations.length === 0) {
+        console.log(`No current configuration.`);
+        return;
+    }
+
+    console.log(`\n\tDeveloper configuration`);
+
+    for (const configuration of configurations) {
+        const {
+            identonym,
+            server,
+            isDefault,
+            projects,
+        } = configuration;
+
+        console.log(`\n\t${server} - ${identonym}${isDefault ? ' [default]' : ''}`);
+
+        if (projects.length === 0) {
+            console.log(`\t\tno projects`);
+            continue;
+        }
+
+        for (const project of projects) {
+            console.log(`\t\t${project.name}`);
+        }
+    }
 }
 // #endregion module
 
