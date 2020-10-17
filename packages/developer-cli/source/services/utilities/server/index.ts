d325949505395b880e4dc7d970dd24e3229a2a1b
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-17 09:41:36 +0300

    developer-cli > dev: services/utilities/server

--- packages/developer-cli/source/services/utilities/server/index.ts
+++ packages/developer-cli/source/services/utilities/server/index.ts
@@ -1,6 +1,32 @@
+// #region imports
+    // #region libraries
+    import {
+        Monitor,
+    } from 'forever-monitor';
+    // #endregion libraries
+// #endregion imports
+
+
+
 // #region module
-const serverStart = () => {
-    const port = 556677;
+const serverStart = async () => {
+    const server = new Monitor('server.js', {
+        max: 3,
+        silent: true,
+        args: []
+    });
+
+    server.start();
+
+    const port = await new Promise((resolve, reject) => {
+        server.on('port', (error, data) => {
+            if (error) {
+                reject();
+            }
+
+            resolve(data);
+        });
+    });
 
     return port;
 }
