b495f43c767550a18fee9b2006343dd98c4160d3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-10-17 09:43:13 +0300

    developer-cli > dev: server

---
+++ packages/developer-cli/source/server.ts
@@ -0,0 +1,18 @@
+// #region imports
+    // #region internal
+    import Server from '#objects/Server';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region module
+const main = () => {
+    const server = new Server();
+
+    server.start();
+}
+
+
+main();
+// #endregion module
