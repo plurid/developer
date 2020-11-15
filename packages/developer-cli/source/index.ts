06afdbabb6f14dce01d6d21fcdf0525279890d3f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-15 10:48:51 +0200

    developer-cli > dev: exports

--- packages/developer-cli/source/index.ts
+++ packages/developer-cli/source/index.ts
@@ -1,13 +1,20 @@
 // #region imports
     // #region internal
     import cli from './cli';
+
+    import * as commands from './commands';
     // #endregion internal
 // #endregion imports
 
 
 
 // #region module
+export * from './data/constants';
+export * from './data/interfaces';
+
+
 export {
     cli,
+    commands,
 };
 // #endregion module
