7e4f3e6fb2ea60d7321e64c851614852046389fc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-16 14:36:18 +0200

    developer-cli > setup: package

---
+++ packages/developer-cli/source/objects/Dashboard/server/logic/loader/index.ts
@@ -0,0 +1,31 @@
+// #region imports
+    // #region external
+    import database from '#server/services/database';
+    // #endregion external
+// #endregion imports
+
+
+
+// #region module
+const loadData = async (
+    ownerID: string | undefined,
+) => {
+    if (!ownerID) {
+        return {
+        };
+    }
+
+
+    const data = {
+
+    };
+
+    return data;
+}
+// #endregion module
+
+
+
+// #region exports
+export default loadData;
+// #endregion exports
