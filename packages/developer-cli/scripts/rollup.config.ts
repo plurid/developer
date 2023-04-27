5bd02312abdc5904e9434d095f5c10920636af76
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-04-27 14:26:57 +0300

    developer-cli > setup: package

--- packages/developer-cli/scripts/rollup.config.ts
+++ packages/developer-cli/scripts/rollup.config.ts
@@ -3,7 +3,7 @@
     import commonjs from '@rollup/plugin-commonjs';
     import typescript from 'rollup-plugin-typescript2';
     import json from '@rollup/plugin-json';
-    import { terser } from 'rollup-plugin-terser';
+    import terser from '@rollup/plugin-terser';
     // #endregion libraries
 
 
